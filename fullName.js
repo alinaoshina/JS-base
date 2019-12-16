0f1233579235ff1468b0c1e63afb008c299c476c
alinaoshina <oshina.a@gmail.com>
2019-12-16 19:59:47 +0200

    Full name variations

--- fullName.js
+++ fullName.js
@@ -2,3 +2,11 @@ const firstName = 'Петя';
 const lastName = 'Иванов';
 const fullName = `${firstName} ${lastName}`;
 console.log(fullName);
+
+const user = {
+    firstName: 'Vasya',
+    lastName: 'Ivanov'
+}
+
+const userOutput = `${user.firstName} ${user.lastName}`
+console.log(userOutput);
