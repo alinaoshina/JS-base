f99aba955a25db2451b8e05078e44e8896d80d5b
alinaoshina <oshina.a@gmail.com>
2019-12-24 21:34:14 +0200

    Accomplished input exercise

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const textArea = document.querySelector('#text-input');
 const button = document.querySelector('#submit-button');
-
+const coloring = document.querySelector('.coloring');
 let value = ''
-
-textArea.addEventListener('change', (event) => {
-    value = event.target.value
-});
-
+let counter = 1;
 const myOutput = document.querySelector('.output');
-button.addEventListener('click', () => {
-    myOutput.innerText = value    
+let coloredDiv = myOutput.childNodes;
+coloring.addEventListener('change', (event) => {
+    for (let i=0; i < coloredDiv.length; i++) {
+        if (counter % 2 === 0) {
+            if (i % 2 === 0) {
+                coloredDiv[i].style.background = event.target.value; 
+            }
+        } else {
+            if (i % 2 !== 0) {
+                console.log(coloredDiv[i])
+                coloredDiv[i].style.background = event.target.value; 
+            }
+        }
+    }
+    counter++; 
+})
+textArea.addEventListener('input', (event) => {
+    value = event.target.value;
+        myOutput.innerHTML = '';
+    for (let i = 0; i < value; i++) {
+        const coloredDiv = document.createElement('div');
+        coloredDiv.innerHTML = i+1;
+        coloredDiv.classList = 'myClass';
+        myOutput.appendChild(coloredDiv);
+               
+    }
 });
