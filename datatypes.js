133d4afe803b764ce04a95ad694eb719029a34c9
alinaoshina <oshina.a@gmail.com>
2019-12-31 14:11:07 +0200

    Arrays

--- datatypes.js
+++ datatypes.js
@@ -1,8 +1,20 @@
-let num = 12;
-let text = String(num);
-let text2 = num.toString();
-let text3 = `${num}`
+let myArray = Array.from(new Array(10),(val,index)=>index+1);;
+let arrayString = myArray.join('/');
 
-let r = Math.ceil(Math.random()*10);
+let myArrayBack = arrayString.split('/');
+myArrayBack = myArrayBack.map(Number);
+console.log(myArrayBack);
 
-console.log(r);
+mySum = myArrayBack[2]+myArrayBack[7];
+console.log(mySum);
+
+let totalSum = 0;
+for (i=0; i<myArrayBack.length; i++) {
+    totalSum += myArrayBack[i];
+}
+console.log(totalSum);
+
+let square = function(num) {
+    return num*num;
+}
+console.log(myArrayBack.map(square));
